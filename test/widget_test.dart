3e3ca9a35bcfaad1bddaab7325ad5815b6dc652a
csnoboa <caique2noboa@gmail.com>
2021-03-31 18:48:47 -0300

    first buttons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,10 +13,10 @@ import 'package:quiz_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(PerguntaApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('5'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
@@ -24,7 +24,7 @@ void main() {
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('5'), findsNothing);
+    expect(find.text('6'), findsOneWidget);
   });
 }
